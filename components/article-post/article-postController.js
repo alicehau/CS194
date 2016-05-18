cee15e435dd2e784edd7bb7c5d7ccc0ae8ac3974
alicehau <alicehau@stanford.edu>
2016-05-17 23:32:30 -0700

    article post and login register components added

---
+++ components/article-post/article-postController.js
@@ -0,0 +1,25 @@
+'use strict';
+
+cs142App.controller('ArticlePostController', ['$scope', '$routeParams', 
+  '$location', '$firebaseArray',
+  function($scope, $routeParams, $location, $firebaseArray) {
+    /*
+     * Since the route is specified as '/photos/:userId' in $routeProvider config the
+     * $routeParams  should have the userId property set with the path from the URL.
+     */
+    $scope.main = {};
+    var articlesRef = new Firebase("https://nooz.firebaseio.com/articles");
+    $scope.main.articles = $firebaseArray(articlesRef);
+    $scope.addArticle = function() {
+      $scope.main.articles.$add({
+        url: $scope.articleLink,
+        comment: $scope.articleComment,
+        title: $scope.articleTitle
+      });
+      //form fields bound to $scope
+      $scope.articleTitle = '';
+      $scope.articleLink = '';
+      $scope.articleComment = '';
+    };
+  }
+]);
---
+++ components/login-register/login-registerController.js
@@ -0,0 +1,71 @@
+'use strict';
+
+cs142App.controller('LoginRegisterController', ['$scope', '$rootScope', 
+  '$location', '$http', 'Auth', '$firebaseArray',
+  function($scope, $rootScope, $location, $http, Auth, $firebaseArray) {
+    /*
+     * Since the route is specified as '/users/:userId' in $routeProvider config the
+     * $routeParams  should have the userId property set with the path from the URL.
+     */
+
+    $scope.main = {};
+
+    //text typed into input box
+    $scope.main.userEmail = "";
+    $scope.main.userPassword = "";
+    $scope.main.newUser = {
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+      passwordReentry: ""
+    };
+
+    $scope.main.login = function() {
+      var ref = new Firebase("https://nooz.firebaseio.com");
+      ref.authWithPassword({
+        email    : $scope.main.userEmail,
+        password : $scope.main.userPassword
+      }, function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+        
+      }
+      });
+    };
+
+    $scope.main.register = function() {
+      $scope.message = null;
+      $scope.error = null;
+      for (var key in $scope.main.newUser) {
+        if ($scope.main.newUser[key].length === 0) {
+          $scope.main.errorMessage = "Please fill out all forms";
+          return;
+        }
+      }
+
+      if ($scope.main.newUser.password !== $scope.main.newUser.passwordReentry) {
+        $scope.main.errorMessage = "Passwords did not match";
+        return;
+      }
+
+      Auth.$createUser({
+        email: $scope.main.newUser.email,
+        password: $scope.main.newUser.password
+      }).then(function(userData) {
+        $scope.main.message = "User created with uid: " + userData.uid;
+              console.log("just registered a user id " + userData.uid);
+        var usersRef = new Firebase("https://nooz.firebaseio.com/users/" + userData.uid + '/');
+        $scope.main.users = $firebaseArray(usersRef);
+        $scope.main.users.$add({
+          first_name: $scope.main.newUser.first_name,
+          last_name: $scope.main.newUser.last_name
+        });
+      }).catch(function(error) {
+        $scope.error = error;
+      });
+    };
+  }
+]);
