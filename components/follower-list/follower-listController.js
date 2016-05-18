5137abd833df91c35196c4296fc306a691862b4e
ybrikeeg <kirbygee@stanford.edu>
2016-05-17 22:30:54 -0700

    Redesigning article cards

--- components/follower-list/follower-listController.js
+++ components/follower-list/follower-listController.js
@@ -5,8 +5,7 @@ function($scope) {
   var currentUser = $scope.shared.currentUser;
   $scope.main = {};
   $scope.main.followingIndex = 0;
-  //track which index of following entry we are hovering over, null by default
-  $scope.main.hoverIndex = null;
+
   //list of users george follows
   $scope.main.following = $scope.shared.currentUser.following;
 
@@ -19,13 +18,10 @@ function($scope) {
     $scope.main.followeeArticles = currentUser.following[$scope.main.followingIndex].reposted_articles;
   }
 
-  /**
-  * Set the class name for a following entry
-  */
-  $scope.main.getClassNameForHeadshot = function(value){
-    console.log("called");
-    if (value === $scope.main.hoverIndex){
-      return "hover";
+
+  $scope.main.highlight = function(index){
+    if (index === $scope.main.followingIndex){
+      return "highlight";
     }
     return "";
   };
@@ -36,15 +32,6 @@ function($scope) {
   $scope.main.followingClickHandler = function(value){
     $scope.main.followingIndex = value;
     updateVisibleArticle();
-  };
-
-  /**
-  * Set value of index of following entry user is hovered over
-  */
-  $scope.main.followingHover = function(value){
-    var id = "#headshot" + value;
-    console.log("hover" + id);
-    $scope.main.hoverIndex = value;
   };
 
   updateVisibleArticle();
