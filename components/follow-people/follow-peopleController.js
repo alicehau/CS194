c3d8e5826d180bb3aaf8043cf855b3e7cecc89d1
alicehau <alicehau@stanford.edu>
2016-05-30 00:10:47 -0700

    updated follow people to exclude self and from following twice

--- components/follow-people/follow-peopleController.js
+++ components/follow-people/follow-peopleController.js
@@ -13,15 +13,40 @@ cs142App.controller('FollowPeopleController', ['$scope', '$routeParams',
     $scope.main.users = $firebaseArray(usersRef);
     var curatorsRef = new Firebase("https://nooz.firebaseio.com/users/" + $scope.currUid + "/following")
     $scope.main.curators = $firebaseArray(curatorsRef);
+    $scope.main.leftToFollow = [];
+    $scope.main.curators.$loaded()
+    .then(function(){
+      $scope.main.users.$loaded().then(function(){
+        getUsersLeftToFollow();
+      });
+    });
 
     $scope.main.followUser = function(followId) {
       var followingRef = new Firebase("https://nooz.firebaseio.com/users/" + $scope.currUid + '/following');
       var following = $firebaseArray(followingRef);
       following.$add(followId).then(function() {
           console.log('Successfully followed! ');
+          getUsersLeftToFollow();
         }).catch(function(error) {
           console.log('Error trying to follow!');
         });
     };
+
+
+    function getUsersLeftToFollow() {
+      $scope.main.leftToFollow = [];
+      console.log("IN HERE!");
+      angular.forEach($scope.main.users, function(user) {
+          var isCurator = false;
+          angular.forEach($scope.main.curators, function(curator) {
+            if (user.$id == curator.$value) {
+              isCurator = true;
+            }
+          });
+          if (!isCurator && user.$id != $scope.shared.authData.uid) {
+            $scope.main.leftToFollow.push(user);
+          }
+        });
+    }
   }
 ]);
