6b09811d65c6d4d59c91b2697409edfdeea56f82
Matthew Leong <mjbleong@stanford.edu>
2016-06-06 03:44:59 -0700

    random shit

--- components/follow-people/follow-peopleController.js
+++ components/follow-people/follow-peopleController.js
@@ -1,8 +1,8 @@
 'use strict';
 
 cs142App.controller('FollowPeopleController', ['$scope', '$routeParams', 
-  '$location', '$firebaseArray',
-  function($scope, $routeParams, $location, $firebaseArray) {
+  '$location', '$firebaseArray', '$http',
+  function($scope, $routeParams, $location, $firebaseArray, $http) {
     /*
      * Since the route is specified as '/photos/:userId' in $routeProvider config the
      * $routeParams  should have the userId property set with the path from the URL.
@@ -51,5 +51,35 @@ cs142App.controller('FollowPeopleController', ['$scope', '$routeParams',
           }
         });
     }
+
+
+
+
+    $scope.addArticle = function() {
+      var ref = new Firebase("https://nooz.firebaseio.com");
+      var authData = ref.getAuth();
+      var articlesRef = new Firebase("https://nooz.firebaseio.com/users/" + authData.uid + "/articles/");
+      $scope.main.articles = $firebaseArray(articlesRef);
+
+      var encodedURI = encodeURIComponent($scope.articleLink);
+
+      $http.get("http://api.diffbot.com/v3/article?token=90d8a5387254ad57971dc32a4a59e9b2&url=" + encodedURI)
+          .then(function(response) {
+              var obj = response.data;
+              $scope.articleData = obj['objects'][0]["html"];
+              $scope.articleTitle = obj['objects'][0]['title'];
+              console.log("Success response");
+              
+              obj['objects'][0].comment = $scope.articleComment;
+              obj['objects'][0].timestamp = Firebase.ServerValue.TIMESTAMP;
+              $scope.main.articles.$add(obj['objects'][0]);
+
+              $scope.articleTitle = '';
+              $scope.articleLink = '';
+              $scope.articleComment = '';
+
+              $location.path("/userPosts");
+      });
+    };
   }
 ]);
