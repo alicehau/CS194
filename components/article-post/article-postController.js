33bad736aa4912e4af5fc73de9db15389a415945
alicehau <alicehau@stanford.edu>
2016-06-06 12:56:08 -0700

    did some styling for profile tab. added ability to post on my posts

--- components/article-post/article-postController.js
+++ components/article-post/article-postController.js
@@ -13,23 +13,22 @@ cs142App.controller('ArticlePostController', ['$scope', '$routeParams',
     var articlesRef = new Firebase("https://nooz.firebaseio.com/users/" + $scope.shared.authData.uid + "/articles/");
     $scope.main.articles = $firebaseArray(articlesRef);
     $scope.addArticle = function() {
-
-      var encodedURI = encodeURIComponent($scope.articleLink);
-
+      var encodedURI = encodeURIComponent($scope.main.articleLink);
+      $scope.main.loading = true;
       $http.get("http://api.diffbot.com/v3/article?token=90d8a5387254ad57971dc32a4a59e9b2&url=" + encodedURI)
           .then(function(response) {
               var obj = response.data;
-              $scope.articleData = obj['objects'][0]["html"];
-              $scope.articleTitle = obj['objects'][0]['title'];
+              $scope.main.articleData = obj['objects'][0]["html"];
+              $scope.main.articleTitle = obj['objects'][0]['title'];
               console.log("Success response");
               
-              obj['objects'][0].comment = $scope.articleComment;
+              obj['objects'][0].comment = $scope.main.articleComment;
               obj['objects'][0].timestamp = Firebase.ServerValue.TIMESTAMP;
               $scope.main.articles.$add(obj['objects'][0]);
 
-              $scope.articleTitle = '';
-              $scope.articleLink = '';
-              $scope.articleComment = '';
+              $scope.main.articleLink = '';
+              $scope.main.articleComment = '';
+              $scope.main.loading = false;
       });
     };
   }
